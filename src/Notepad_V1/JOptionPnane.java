272e85f51efb14cd55b25135b57c5f5f1a7cb3c4
mike <mike@arch-linux>
2022-11-24 19:21:27 +0300

    First commit

---
+++ src/Notepad_V1/JOptionPnane.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Notepad_V1;
+
+/**
+ *
+ * @author mike
+ */
+class JOptionPnane {
+
+    static void showMessageDialog(Object object, String note_saved_into_wallet) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
